cb05a90bc8c1ecd098fd019649aa78a1791090bd
sloki274 <64668410+sloki274@users.noreply.github.com>
2020-06-30 22:12:51 -0400

    Add files via upload

---
+++ scanner.java
@@ -0,0 +1,28 @@
+package scanner;
+import java.util.Scanner;
+public class scanner {
+	public static void main(String[] args) {
+		
+		Scanner ob =new Scanner(System.in);
+		
+		System.out.println("enter two value1");
+		int num = ob.nextInt();
+
+		System.out.println("the value1 is "+num);
+		System.out.println("enter two value2");
+		int num2 = ob.nextInt();
+
+		System.out.println("the value2 is "+num2);
+		
+
+	
+		
+	}
+
+	public void add(int num, int num2) {
+		
+		System.out.println();
+		
+		
+	}
+}
